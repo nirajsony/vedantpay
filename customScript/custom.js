67c53a760c2dc04486476a4e8b19830abc519da9
Niraj Soni <nirajsony321@yahoo.com>
2021-07-08 16:59:05 +0530

    Add Slider in home page

--- customScript/custom.js
+++ customScript/custom.js
@@ -5,4 +5,42 @@
     { // When page is fully loaded...
       $('.precontainer').hide(); // hide preloader
       $('#vedcontent').show(); // and show content
+
+         // Script for Service Page starts here
+   $('.autoplay').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+        
     });
